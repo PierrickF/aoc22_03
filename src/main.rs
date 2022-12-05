2d75a271c3af4c85b236bcf3065f224f3405283e
Pierrick Foulquier <pierrickfoulquier@gmail.com>
2022-12-05 16:23:00 +0100

    string_to_vector() now converts the entire string

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,14 @@ fn parse_file<R: Read>(input: R) {
     for line in br.lines() {
         let str_line = line.expect("Failed to read line");
         let vec = string_to_vector(str_line);
+        println!("{:?}", vec);
     }
 }
 
 fn string_to_vector(line: String) -> Vec<char> {
     let mut iter = line.chars();
     let mut vec: Vec<char> = Vec::new();
-    for _ in 0..3 {
+    for _ in 0..line.chars().count() {
         vec.push(iter.next().unwrap());
     }
     vec

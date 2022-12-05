ea32d93ee209e1134aaaa32ab36b409cd51d65db
Pierrick Foulquier <pierrickfoulquier@gmail.com>
2022-12-05 12:51:56 +0100

    Add parse_file() and string_to_vector()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, Read};
+
+fn main() -> Result<(), Error> {
+    parse_file(File::open("input")?);
+    Ok(())
+}
+
+fn parse_file<R: Read>(input: R) {
+    let br = BufReader::new(input);
+    for line in br.lines() {
+        let str_line = line.expect("Failed to read line");
+        let vec = string_to_vector(str_line);
+    }
+}
+
+fn string_to_vector(line: String) -> Vec<char> {
+    let mut iter = line.chars();
+    let mut vec: Vec<char> = Vec::new();
+    for _ in 0..3 {
+        vec.push(iter.next().unwrap());
+    }
+    vec
 }

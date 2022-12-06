bbbf3d7a1bfcdb2726c3f4542f2c03d0788129da
Pierrick Foulquier <pierrickfoulquier@gmail.com>
2022-12-06 12:02:45 +0100

    Add example input + print task 1 correct answer

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,17 @@ fn main() -> Result<(), Error> {
 
 fn parse_file<R: Read>(input: R) {
     let br = BufReader::new(input);
+    let mut total: usize = 0;
     for line in br.lines() {
         let str_line = line.expect("Failed to read line");
         let vec = string_to_vector(str_line);
         let (vec1, vec2) = split_vec(&vec);
-        println!("\n{:?}", vec);
-        println!("{:?}", vec1);
-        println!("{:?}", vec2);
+        if find_duplicate((&vec1, &vec2)) != Err(false) {
+            let dup = find_duplicate((&vec1, &vec2)).unwrap();
+            total = dup_to_val(dup) + total;
+        }
     }
+    println!("{total}");
 }
 
 fn string_to_vector(line: String) -> Vec<char> {
@@ -27,12 +30,27 @@ fn string_to_vector(line: String) -> Vec<char> {
     vec
 }
 
-fn parse_line(vec: Vec<char>) -> u32 {
-    23
+fn find_duplicate(vecs: (&Vec<char>, &Vec<char>)) -> Result<char, bool> {
+    for i in vecs.0 {
+        if vecs.1.contains(&i) {
+            return Ok(*i);
+        } else {
+            continue
+        }
+    }
+    Err(false)
 }
 
 fn split_vec(vec: &Vec<char>) -> (Vec<char>, Vec<char>) {
-    let vec1: Vec<char> = vec[0..vec.len()/2].to_vec();
-    let vec2: Vec<char> = vec[vec.len()/2..vec.len()].to_vec();
+    let vec1: Vec<char> = vec[0..vec.len() / 2].to_vec();
+    let vec2: Vec<char> = vec[vec.len() / 2..vec.len()].to_vec();
     (vec1, vec2)
+}
+
+fn dup_to_val(dup: char) -> usize {
+    let mut low_alph: Vec<char> = ('a'..='z').collect();
+    let mut upp_alph: Vec<char> = ('A'..='Z').collect();
+    low_alph.append(&mut upp_alph);
+    let val = low_alph.iter().position(|&r| r == dup).unwrap() + 1;
+    val
 }
